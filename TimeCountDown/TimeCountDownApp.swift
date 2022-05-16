3527fa4ce9524840fbec3ac40c6c04bfc2f18dd8
Enryun <baokieuha@gmail.com>
2022-05-16 12:54:59 +0700

    Initial Commit

---
+++ TimeCountDown/TimeCountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCountDownApp.swift
+//  TimeCountDown
+//
+//  Created by James Thang on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeCountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
